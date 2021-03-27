86edb837576436b858c01ecf1897b5dc59297c49
Seth Torek <seth.torek@gmail.com>
2021-03-27 14:27:09 -0400

    added icons

--- assets/script.js
+++ assets/script.js
@@ -7,48 +7,128 @@ $("#currentHour").text(today.format("LT"));
 
 
 
-
 // JAVASCRIPT VARIABLES --------------------------------------------------
-var rows = {
-  row1: 23,
-  row2: 10,
-  row3: 11,
-  row4: 12,
-  row5: 13,
-  row6: 14,
-  row7: 15,
-  row8: 16,
-  row9: 17,
-}
 
-// var i = [9,10,11,12,13,14,15,16,17,]
-
-// how to assign i to  rows object key value???????????????
+  var time1 = 8;
+  var time2 = 9;
+  var time3 = 10;
+  var time4 = 11;
+  var time5 = 12;
+  var time6 = 13;
+  var time7 = 14;
+  var time8 = 15;
+  var time9 = 16;
 
 
-for(i = 0; i < rows.value; i++) {
-  var rows = i
-}
-// ASSIGN the for loop to a variable then insert that variable into the function
-// need to loop through rows object key value??
+
+   
+
+
+
 
 // FUNCTION DEFINITIONS --------------------------------------------------
 
+function updateRow1() {
+  var hr = (new Date()).getHours()
+  if (hr < time1) {
+    $( ".row1" ).addClass( "future" );
+  } else if (hr > time1) {
+      $( ".row1" ).addClass( "past" );
+  } else if (hr == time1) {
+      $( ".row1" ).addClass( "present" );
+  } 
+};
+
+function updateRow2() {
+  var hr = (new Date()).getHours()
+  if (hr < time2) {
+    $( ".row2" ).addClass( "future" );
+  } else if (hr > time2) {
+      $( ".row2" ).addClass( "past" );
+  } else if (hr == time2) {
+      $( ".row2" ).addClass( "present" );
+  } 
+};
+
+function updateRow3() {
+  var hr = (new Date()).getHours()
+  if (hr < time3) {
+    $( ".row3" ).addClass( "future" );
+  } else if (hr > time3) {
+      $( ".row3" ).addClass( "past" );
+  } else if (hr == time3) {
+      $( ".row3" ).addClass( "present" );
+  } 
+};
+
+function updateRow4() {
+  var hr = (new Date()).getHours()
+  if (hr < time4) {
+    $( ".row4" ).addClass( "future" );
+  } else if (hr > time4) {
+      $( ".row4" ).addClass( "past" );
+  } else if (hr == time4) {
+      $( ".row4" ).addClass( "present" );
+  } 
+};
+
+function updateRow5() {
+  var hr = (new Date()).getHours()
+  if (hr < time5) {
+    $( ".row5" ).addClass( "future" );
+  } else if (hr > time5) {
+      $( ".row5" ).addClass( "past" );
+  } else if (hr == time5) {
+      $( ".row5" ).addClass( "present" );
+  } 
+};
+
+function updateRow6() {
+  var hr = (new Date()).getHours()
+  if (hr < time6) {
+    $( ".row6" ).addClass( "future" );
+  } else if (hr > time6) {
+      $( ".row6" ).addClass( "past" );
+  } else if (hr == time6) {
+      $( ".row6" ).addClass( "present" );
+  } 
+};
+
+function updateRow7() {
+  var hr = (new Date()).getHours()
+  if (hr < time7) {
+    $( ".row7" ).addClass( "future" );
+  } else if (hr > time7) {
+      $( ".row7" ).addClass( "past" );
+  } else if (hr == time7) {
+      $( ".row7" ).addClass( "present" );
+  } 
+};
+
+function updateRow8() {
+  var hr = (new Date()).getHours()
+  if (hr < time8) {
+    $( ".row8" ).addClass( "future" );
+  } else if (hr > time8) {
+      $( ".row8" ).addClass( "past" );
+  } else if (hr == time8) {
+      $( ".row8" ).addClass( "present" );
+  } 
+};
+
+function updateRow9() {
+  var hr = (new Date()).getHours()
+  if (hr < time9) {
+    $( ".row9" ).addClass( "future" );
+  } else if (hr > time9) {
+      $( ".row9" ).addClass( "past" );
+  } else if (hr == time9) {
+      $( ".row9" ).addClass( "present" );
+  } 
+};
 
 
-function updateRowColor() {
-    var hr = (new Date()).getHours()
-    if (hr < i) {
-      $( "textarea" ).addClass( "future" );
-    } else if (hr > i) {
-        $( "textarea" ).addClass( "past" );
-    } else if (hr == i) {
-        $( "textarea" ).addClass( "present" );
-    } 
-  }
-  
-  setInterval(updateRowColor, 1000 * 60);
-  updateRowColor();
+
 
 
 // EVENT LISTENERS --------------------------------------------------------
@@ -57,4 +137,30 @@ function updateRowColor() {
 
 
 // FUNCTION CALLS ---------------------------------------------------------
-updateRowColor();
+
+setInterval(updateRow1, 1000 * 60);
+updateRow1();
+
+setInterval(updateRow2, 1000 * 60);
+updateRow2();
+
+setInterval(updateRow3, 1000 * 60);
+updateRow3();
+
+setInterval(updateRow4, 1000 * 60);
+updateRow4();
+
+setInterval(updateRow5, 1000 * 60);
+updateRow5();
+
+setInterval(updateRow6, 1000 * 60);
+updateRow6();
+
+setInterval(updateRow7, 1000 * 60);
+updateRow7();
+
+setInterval(updateRow8, 1000 * 60);
+updateRow8();
+
+setInterval(updateRow9, 1000 * 60);
+updateRow9();
